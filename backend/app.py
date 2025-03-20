f7bf1f16e18eea893351fab104ec178cd0bdb0f8
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-03-20 18:28:42 -0400

    Fixed Create method.

--- backend/app.py
+++ backend/app.py
@@ -10,16 +10,24 @@ def home():
 @app.route("/create_data_entry", methods=["POST"])
 def create_entry():
     data = request.get_json()
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    message = data.get("message")
+    email = data.get("email")
 
-    dataModel = MessageModel(**{key: data.get(key) for key in ["first_name", "last_name", "message", "email"]})
+    if not first_name or not last_name or not message or not email:
+        return (jsonify({"message": "You must include a first name, last name, message, and email"}), 400)
 
+    new_message = MessageModel(first_name = first_name, last_name = last_name, message = message, email = email)
+    
     try:
-        db.add(dataModel)
-        db.commit()
-        return jsonify({"message": "Successfully created data entry!"}), 201
+        db.session.add(new_message)
+        db.session.commit()
     except Exception as e: 
-        db.rollback()
-        return jsonify({"message": str(e)}), 500
+        db.session.rollback()
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Data entry successfully added!"}), 201
 
 # Read
 @app.route("/retrieve_data", methods=["GET"])
